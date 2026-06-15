e77a0f5e2c8b82cbad8a6cca4addb91510a99f35
Florian <flo.viret@gmail.com>
2026-06-15 11:09:06 +0200

    Initial Commit

---
+++ Podomètre/Podome_treApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podome_treApp.swift
+//  Podomètre
+//
+//  Created by Flo Viret on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Podome_treApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodomètreTests/Podome_treTests.swift
@@ -0,0 +1,17 @@
+//
+//  Podome_treTests.swift
+//  PodomètreTests
+//
+//  Created by Flo Viret on 15/06/2026.
+//
+
+import Testing
+@testable import Podome_tre
+
+struct Podome_treTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
